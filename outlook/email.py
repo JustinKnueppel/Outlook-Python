2c49a54636644cc702882326607642a04cf2876f
Knueppel, Justin <knuepj1@nationwide.com>
2019-07-02 13:02:34 -0400

    Write basic interface for email

---
+++ outlook/email.py
@@ -0,0 +1,34 @@
+import win32com
+import win32com.client
+from datetime import datetime
+
+from typing import List
+
+class Email:
+    def __init__(self, win32_email: win32com.client.CDispatch):
+        self._email = win32_email
+
+    def attachments(self) -> List[win32com.client.CDispatch]:
+        """Return a list of attachments"""
+        num_attachments = self._email.Attachments.Count
+        return [self._email.Attachments.Item(i) for i in range(num_attachments)]
+
+    def body(self) -> str:
+        """Returns the body of the email"""
+        return str(self._email.Body)
+
+    def received_time(self) -> datetime:
+        """Returns the time the email was received"""
+        return self._email.RecievedTime
+
+    def sender_email(self) -> str:
+        """Returns the email address of the sender"""
+        return str(self._email.SenderEmailAddress)
+
+    def sender_name(self) -> str:
+        """Returns the name of the sender"""
+        return str(self._email.SenderName)
+
+    def subject(self) -> str:
+        """Returns the subject line of the email"""
+        return str(self._email.Subject)
