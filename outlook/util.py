3895548dd6c09af1e1e1fa33e32fa708d4992e2d
Knueppel, Justin <knuepj1@nationwide.com>
2019-07-02 14:40:09 -0400

    Add sender email and make case insensitive

--- outlook/util.py
+++ outlook/util.py
@@ -3,28 +3,33 @@ from .email import Email
 
 from itertools import compress
 
-def search(emails: List[Email], senders: List[str]=[], subjects: List[str]=[], bodies: List[str]=[], attachments: List[str]=[]) -> List[Email]:
+def search(emails: List[Email], sender_names: List[str]=[], sender_emails: List[str]=[], subjects: List[str]=[], bodies: List[str]=[], attachments: List[str]=[]) -> List[Email]:
     """Return a subset of emails containing any of the given criteria"""
     matches = [False]*len(emails)
 
-    if senders:
-        # Check if any of the given senders appear
-        sender_matches = [any(sender in email.sender() for sender in senders) for email in emails]
-        matches = [x or y for x, y in zip(matches, sender_matches)]
+    if sender_names:
+        # Check if any of the given sender names appear
+        sender_name_matches = [any(sender.lower() in email.sender_name().lower() for sender in sender_names) for email in emails]
+        matches = [x or y for x, y in zip(matches, sender_name_matches)]
+
+    if sender_emails:
+        # Check if any of the given sender emails appear
+        sender_email_matches = [any(sender.lower() in email.sender_email().lower() for sender in sender_emails) for email in emails]
+        matches = [x or y for x, y in zip(matches, sender_email_matches)]
 
     if subjects:
         # Check if any of the given subjects appear
-        subject_matches = [any(subject in email.subject() for subject in subjects) for email in emails]
+        subject_matches = [any(subject.lower() in email.subject().lower() for subject in subjects) for email in emails]
         matches = [x or y for x, y in zip(matches, subject_matches)]
 
     if bodies:
         # Check if any of the given bodies appear
-        body_matches = [any(body in email.body() for body in bodies) for email in emails]
+        body_matches = [any(body.lower() in email.body().lower() for body in bodies) for email in emails]
         matches = [x or y for x, y in zip(matches, body_matches)]
 
     if attachments:
         # Check if any of the given attachment names appear in any attachments
-        attachment_matches = [any(any(attachment_target in attachment.name() for attachment_target in attachments) for attachment in email.attachments()) for email in emails]
+        attachment_matches = [any(any(attachment_target.lower() in attachment.name().lower() for attachment_target in attachments) for attachment in email.attachments()) for email in emails]
         matches = [x or y for x, y in zip(matches, attachment_matches)]
 
     return list(compress(emails, matches))
