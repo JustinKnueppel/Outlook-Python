b33a66da5898754b184cde2e3c13e0cad9b47c3e
Justin <justinknueppel@gmail.com>
2019-07-13 22:55:08 -0400

    Remove redundant search

--- outlook/folder.py
+++ outlook/folder.py
@@ -16,38 +16,6 @@ class Folder:
     def __str__(self):
         return self._folder.Name
 
-    @staticmethod
-    def _search(emails: List[Email], sender_names: List[str]=[], sender_emails: List[str]=[], subjects: List[str]=[], bodies: List[str]=[], attachments: List[str]=[]) -> List[Email]:
-        """Return a subset of emails containing any of the given criteria"""
-        matches = [False]*len(emails)
-
-        if sender_names:
-            # Check if any of the given sender names appear
-            sender_name_matches = [any(sender.lower() in email.sender_name().lower() for sender in sender_names) for email in emails]
-            matches = [x or y for x, y in zip(matches, sender_name_matches)]
-
-        if sender_emails:
-            # Check if any of the given sender emails appear
-            sender_email_matches = [any(sender.lower() in email.sender_email().lower() for sender in sender_emails) for email in emails]
-            matches = [x or y for x, y in zip(matches, sender_email_matches)]
-
-        if subjects:
-            # Check if any of the given subjects appear
-            subject_matches = [any(subject.lower() in email.subject().lower() for subject in subjects) for email in emails]
-            matches = [x or y for x, y in zip(matches, subject_matches)]
-
-        if bodies:
-            # Check if any of the given bodies appear
-            body_matches = [any(body.lower() in email.body().lower() for body in bodies) for email in emails]
-            matches = [x or y for x, y in zip(matches, body_matches)]
-
-        if attachments:
-            # Check if any of the given attachment names appear in any attachments
-            attachment_matches = [any(any(attachment_target.lower() in attachment.name().lower() for attachment_target in attachments) for attachment in email.attachments()) for email in emails]
-            matches = [x or y for x, y in zip(matches, attachment_matches)]
-
-        return list(compress(emails, matches))
-
     def emails(self, recursive: bool=False) -> List[Email]:
         """Return collection of emails in the given folder"""
         emails = [Email(email) for email in self._folder.Items if email.MessageClass == 'IPM.Note']
@@ -59,14 +27,3 @@ class Folder:
     def folders(self) -> List['Folder']:
         """Return collection of subfolders in the given folder"""
         return [Folder(folder) for folder in self._folder.Folders]
-
-    def search(self, recursive=False, **kwargs) -> List[Email]:
-        """Return collection of emails matching the given search critera"""
-        emails = self.emails()
-        matches = Folder._search(emails, **kwargs)
-
-        if recursive:
-            for subfolder in self.folders():
-                matches.extend(subfolder.search(recursive=True, **kwargs))
-        
-        return matches
